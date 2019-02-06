ff817bfb39ecec37cb9b9dd2622930b635c2c16b
Bitte Ruhe <bittequiet@gmail.com>
2019-02-06 19:41:30 +0100

    chapter 6

--- src/chapters/chapter_5/sketch.js
+++ src/chapters/chapter_5/sketch.js
@@ -10,48 +10,8 @@ function setup() {
 
   resetMatrix();
   translate(width / 2, height);
-  new Branch(this.root, height / 6.1, width / 2, 0, 8).draw(8);
+  new Branch(createVector(0,0), height / 6.1, width / 2, 0, 8, color(163,199,244), color(163,0,0xff)).draw(8);
   stroke(color(163, 199, 244));
   strokeWeight(6);
   line(0, 0, 0, -height);
-}
-class Branch {
-  constructor(root = createVector(0, 0), scale_y = 10, max_width = width, generation = 0, max_generations = 10, orientation = -1) {
-    this.root = root;
-    this.scale_y = scale_y;
-    this.max_width = max_width;
-    this.generation = generation;
-    this.max_generations = max_generations;
-    this.orientation = orientation;
-    this.target_left = this.root.copy();
-    this.target_right = this.root.copy();
-  }
-
-  draw(children = 0) {
-    let dist_x = this.max_width / 2;
-    let dist_y = this.orientation * this.scale_y;
-    this.target_left.x -= dist_x;
-    this.target_left.y += dist_y;
-    this.target_right.x += dist_x;
-    this.target_right.y += dist_y;
-
-    let white = color(163,0,0xff);
-    let blue = color(163,199,244);
-    //stroke(lerpColor(blue, white, this.generation/this.max_generations));
-    stroke(163,map(this.generation,0,this.max_generations,199, 0x00),map(this.generation,0,this.max_generations,244, 0xff));
-    strokeWeight(6);
-    //line(this.root.x, this.root.y, this.target_left.x, this.target_left.y);
-    //line(this.root.x, this.root.y, this.target_right.x, this.target_right.y);
-    noFill();
-    let bezier_scale = this.scale_y/(this.generation+1);
-    bezier(this.root.x, this.root.y, this.root.x, this.root.y-bezier_scale, this.target_right.x, this.target_right.y+bezier_scale, this.target_right.x, this.target_right.y);
-    bezier(this.root.x, this.root.y, this.root.x, this.root.y-bezier_scale, this.target_left.x, this.target_left.y+bezier_scale, this.target_left.x, this.target_left.y);
-
-
-    if (children > 0) {
-      new Branch(this.target_left, this.scale_y * 0.9, this.max_width / 2, this.generation + 1, this.max_generations).draw(children - 1);
-      new Branch(this.target_right, this.scale_y * 0.9, this.max_width / 2, this.generation + 1, this.max_generations).draw(children - 1);
-    }
-  }
-
 }
--- src/chapters/chapter_6/sketch.js
+++ src/chapters/chapter_6/sketch.js
@@ -4,9 +4,26 @@ function setup() {
   createCanvas(siz, siz / 3);
   background(0, 0, 0xff);
 
-  resetMatrix();
-  translate(width / 2, height / 2);
-  stroke(color(163, 199, 244));
-  strokeWeight(6);
-  line(0, height / 2, 0, -height / 2);
+  translate(width / 2, height);
+  let offset_y = height / 3;
+  let step_x = 6 * 4;
+  for (let i = step_x * 0; i < width-step_x*4; i += step_x) {
+    stroke(163, 199, 244);
+    new Branch(createVector(0, 0), offset_y, i, 0).draw(0);
+    stroke(163, 199, 244);
+    let switch_offset = (((i % (step_x * 2) == step_x) ? -1 : 1) * step_x*0.5);
+    line(- i/2, -offset_y, - i/2, -offset_y*1.5);
+    line( i/2, -offset_y, i/2, -offset_y*1.5);
+    line(- i/2 + switch_offset, -offset_y*2, - i/2 + switch_offset, -height);
+    line( i/2 + switch_offset, -offset_y*2, i/2 + switch_offset, -height);
+
+    let bezier_scale = 30;
+    for(let orienation of [-1,1]){
+    bezier(orienation* i / 2, -offset_y*1.5,
+       orienation*i / 2, -offset_y*1.5 - bezier_scale,
+       orienation*i / 2 + switch_offset,  -offset_y*2 + bezier_scale,
+       orienation*i / 2 + switch_offset,  -offset_y*2);
+     }
+    //bezier(i/2, 0, i/2, 0, i/2, -height, i/2,-height);
+  }
 }
